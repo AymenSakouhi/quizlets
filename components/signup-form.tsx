217764b7ad659a63cb46eafa3e6c8ac48a331f43
Aymen Sakouhi <aymenmarketer@gmail.com>
2025-09-16 21:07:11 +0200

    sign up and sign in forms

---
+++ components/signup-form.tsx
@@ -0,0 +1,68 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function SignUpForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Sign up for a new account</CardTitle>
+          <CardDescription>Enter your future credentials</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="repeatedPassword">Password again</Label>
+                </div>
+                <Input id="repeatedPassword" type="password" required />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  Sign up
+                </Button>
+                <Button variant="outline" className="w-full">
+                  Login with Google instead
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              you have an account?{" "}
+              <a href="#" className="underline underline-offset-4">
+                Sign in
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
